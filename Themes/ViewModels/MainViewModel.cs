b5b84ba3abafd550f0a2439b9f46e8ebd5071fd0
kettl <kettl@DESKTOP-14B6IQ3>
2020-01-28 20:33:00 +0000

    Uploading Stuff

---
+++ Themes/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Themes.Utilities;
+
+namespace Themes.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private int _themeIndex;
+        public int ThemeIndex { get => _themeIndex; set { RaisePropertyChanged(ref _themeIndex, value); SetTheme(); } }
+
+        public MainViewModel()
+        {
+
+        }
+
+        public void SetTheme()
+        {
+            if (ThemeIndex == 0)
+                MainWindow.SetTheme(MainWindow.Theme.Light);
+            else if (ThemeIndex == 1)
+                MainWindow.SetTheme(MainWindow.Theme.Dark);
+        }
+    }
+}
